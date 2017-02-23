ab687751b0a097b034f21924f5cb862a8d7e8553
cgreger <greger@madisoncollege.edu>
2017-02-22 19:00:25 -0600

    Burrito cost correctly calculated

--- burrito-order.js
+++ burrito-order.js
@@ -3,13 +3,14 @@ var receipt = new Receipt();
 function init() {
 
 	var btnAddBurrito = document.getElementById("btnAddBurrito");
-	var type = document.getElementById("burritoType").value;
-	var rice = getRiceType();
-	var beans = getBeanType();
-	var salsas = getSalsaChoices();
-	var guac = document.getElementById("guac").checked;
 
 	btnAddBurrito.onclick = function() {
+
+		var type = document.getElementById("burritoType").value;
+		var rice = getRiceType();
+		var beans = getBeanType();
+		var salsas = getSalsaChoices();
+		var guac = document.getElementById("guac").checked;
 
 		var burrito = new Burrito();
 
@@ -18,12 +19,13 @@ function init() {
 		burrito.beans = beans;
 		burrito.salsas = salsas;
 		burrito.guac = guac;
+		burrito.calculateCost();
 
 	}
 
 	function getSalsaChoices() {
 
-		var salsaChoices = document.getElementById("salsas");
+		var salsaChoices = document.getElementsByName("salsa");
 		var selected = [];
 
 		for (var i = 0; i < salsaChoices.length; i++) {
@@ -70,6 +72,17 @@ function init() {
 
 }
 
+function Menu() {
+
+	this.chickenPrice = 6.20;
+	this.steakPrice = 6.75;
+	this.carnitasPrice = 6.60;
+	this.barbacoaPrice = 6.60;
+	this.vegitarianPrice = 6.20;
+	this.guacPrice = 1.40;
+
+}
+
 function Burrito() {
 
 	this.type;
@@ -77,10 +90,39 @@ function Burrito() {
 	this.beans;
 	this.salsas = [];
 	this.guac;
+	this.cost = 0;
 
 	this.calculateCost = function() {
 
+		var menu = new Menu();
 
+		if (this.type === "chicken") {
+
+			this.cost += menu.chickenPrice;
+
+		} else if (this.type === "steak") {
+
+			this.cost += menu.steakPrice;
+
+		} else if (this.type === "carnitas") {
+
+			this.cost += menu.carnitasPrice;
+
+		} else if (this.type === "barbacoa") {
+
+			this.cost += menu.barbacoaPrice;
+
+		} else if (this.type === "vegitarian") {
+
+			this.cost += menu.vegitarianPrice;
+
+		}
+
+		if (this.guac) {
+
+			this.cost += menu.guacPrice;
+
+		}
 
 	}
 
